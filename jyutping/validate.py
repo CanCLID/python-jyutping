ce5ccdfd42520c03a37841aebf1e92cd416a8d0f
graphemecluster <graphemecluster@gmail.com>
2022-03-21 19:06:10 +0800

    Refactor

--- jyutping/validate.py
+++ jyutping/validate.py
@@ -17,14 +17,6 @@ class Validator:
     def match(matcher, group):
         return matcher is None or group in ['' if part == '-' else part for part in matcher.split(' ')]
 
-    @staticmethod
-    def rules(*status):
-        def generate(level):
-            def fire(*matchers):
-                return (level, matchers)
-            return fire
-        return tuple(generate(level) for level in status)
-
 class ValidationStatus:
     VALID = 0
     UNCOMMON = 1
@@ -52,86 +44,86 @@ def validate_args(*args):
     >>> validate('ng', 'u', 'k', '1')
     2  # ValidationStatus.INVALID
     '''
-    valid, alert, error = Validator.rules(ValidationStatus.VALID, ValidationStatus.UNCOMMON, ValidationStatus.INVALID)
+    valid, alert, error = ValidationStatus.VALID, ValidationStatus.UNCOMMON, ValidationStatus.INVALID
     return Validator(args).test({
         'Onset - Nucleus - Coda': [
-            valid('- h', '-', 'm ng'),
-            error(None, '-', None),
+            (valid, ('- h', '-', 'm ng')),
+            (error, (None, '-', None)),
 
-            error('b p m f gw kw w', 'o', 'i'),
-            valid('z c s j', 'yu', '-'),
-            error(None, 'yu', '-'),
+            (error, ('b p m f gw kw w', 'o', 'i')),
+            (valid, ('z c s j', 'yu', '-')),
+            (error, (None, 'yu', '-')),
 
-            error('f', 'a', 'k'),
-            error('f', 'o', 'u'),
-            error('f d t', 'aa', 'u ng'),
-            error('t', 'e', '- i'),
+            (error, ('f', 'a', 'k')),
+            (error, ('f', 'o', 'u')),
+            (error, ('f d t', 'aa', 'u ng')),
+            (error, ('t', 'e', '- i')),
 
-            alert('w', 'i', '-'),
-            error('w', 'i', 'n t'),
-            error('w', 'u', 'ng k'),
+            (alert, ('w', 'i', '-')),
+            (error, ('w', 'i', 'n t')),
+            (error, ('w', 'u', 'ng k')),
 
-            error('kw', 'aa', 'n'),
-            error('kw', 'a', 'k'),
-            error('kw', 'o', '-'),
-            error('kw', 'i', 'ng'),
+            (error, ('kw', 'aa', 'n')),
+            (error, ('kw', 'a', 'k')),
+            (error, ('kw', 'o', '-')),
+            (error, ('kw', 'i', 'ng')),
 
-            error('j', 'aa', 'm n t'),
-            error('j', 'a', 'ng k'),
+            (error, ('j', 'aa', 'm n t')),
+            (error, ('j', 'a', 'ng k')),
         ],
         'Nucleus - Coda': [
-            valid('l g', 'a', '-'),
-            alert('- m z', 'a', '-'),
-            error(None, 'a', '-'),
+            (valid, ('l g', 'a', '-')),
+            (alert, ('- m z', 'a', '-')),
+            (error, (None, 'a', '-')),
 
-            valid('- g ng h', 'o', 'n'),
-            valid('g h', 'o', 't'),
-            error(None, 'o', 'm n p t'),
-            alert(None, 'e', 'u m p t'),
-            error(None, 'e', 'n'),
+            (valid, ('- g ng h', 'o', 'n')),
+            (valid, ('g h', 'o', 't')),
+            (error, (None, 'o', 'm n p t')),
+            (alert, (None, 'e', 'u m p t')),
+            (error, (None, 'e', 'n')),
 
-            valid(None, 'oe', '- ng k'),
-            valid('z c', 'oe', 't'),
-            error(None, 'oe', None),
-            valid(None, 'eo', 'i n t'),
-            error(None, 'eo', None),
+            (valid, (None, 'oe', '- ng k')),
+            (valid, ('z c', 'oe', 't')),
+            (error, (None, 'oe', None)),
+            (valid, (None, 'eo', 'i n t')),
+            (error, (None, 'eo', None)),
 
-            error(None, 'i', 'i'),
-            error(None, 'u', 'u m p'),
-            valid(None, 'yu', '- n t'),
-            error(None, 'yu', None),
+            (error, (None, 'i', 'i')),
+            (error, (None, 'u', 'u m p')),
+            (valid, (None, 'yu', '- n t')),
+            (error, (None, 'yu', None)),
         ],
         'Onset - Coda': [
-            error('gw kw w', None, 'u'),
-            valid('b', 'a', 'm'),
-            error('b p m f gw kw w', None, 'm p'),
-            error('kw', None, 't'),
+            (error, ('gw kw w', None, 'u')),
+            (valid, ('b', 'a', 'm')),
+            (error, ('b p m f gw kw w', None, 'm p')),
+            (error, ('kw', None, 't')),
         ],
         'Onset - Nucleus': [
-            error('- b p m f ng gw kw w', 'oe eo yu', None),
-            alert('f', 'i', None),
+            (error, ('- b p m f ng gw kw w', 'oe eo yu', None)),
+            (alert, ('f', 'i', None)),
 
-            alert('j', 'o', '-'),
-            error('j', 'o', None),
+            (alert, ('j', 'o', '-')),
+            (error, ('j', 'o', None)),
 
-            valid('-', 'e', '-'),
-            alert('-', 'e', 'i'),
-            error('-', 'e', None),
+            (valid, ('-', 'e', '-')),
+            (alert, ('-', 'e', 'i')),
+            (error, ('-', 'e', None)),
 
-            valid('-', 'u', 'ng k'),
-            alert('-', 'i', 'k'),
-            error('-', 'i u', None),
+            (valid, ('-', 'u', 'ng k')),
+            (alert, ('-', 'i', 'k')),
+            (error, ('-', 'i u', None)),
 
-            alert('- ng gw w', 'e', None),
-            error('kw', 'e', None),
-            valid('gw kw', 'i', 'ng k'),
-            error('ng gw kw', 'i u', None),
+            (alert, ('- ng gw w', 'e', None)),
+            (error, ('kw', 'e', None)),
+            (valid, ('gw kw', 'i', 'ng k')),
+            (error, ('ng gw kw', 'i u', None)),
 
-            valid('l', 'u', '-'),
-            valid('d t n l h z c s j', 'u', 'ng k'),
-            error('d t n l h z c s j', 'u', None),
+            (valid, ('l', 'u', '-')),
+            (valid, ('d t n l h z c s j', 'u', 'ng k')),
+            (error, ('d t n l h z c s j', 'u', None)),
         ],
         'Other': [
-            alert(None, None, 'p t k', '4 5'),
+            (alert, (None, None, 'p t k', '4 5')),
         ],
     })
